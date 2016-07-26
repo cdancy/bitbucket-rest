be8e2151e52e65e151e691884a193b86780ae860
Tomas Bjerre <tomas.bjerre85@gmail.com>
2016-07-26 15:57:10 +0200

    Avoiding star import
     * As it is forbidden in Checkstyle config. To make build pass.

--- src/main/java/com/cdancy/bitbucket/rest/features/BranchApi.java
+++ src/main/java/com/cdancy/bitbucket/rest/features/BranchApi.java
@@ -28,13 +28,17 @@ import org.jclouds.rest.annotations.Fallback;
 import org.jclouds.rest.annotations.Payload;
 import org.jclouds.rest.annotations.PayloadParam;
 import org.jclouds.rest.annotations.RequestFilters;
-
 import org.jclouds.rest.binders.BindToJsonPayload;
 
 import javax.inject.Named;
-
-import javax.ws.rs.*;
-
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Produces(MediaType.APPLICATION_JSON)
