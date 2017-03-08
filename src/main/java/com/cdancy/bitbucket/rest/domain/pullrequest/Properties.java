d901b9ea044e74e3303c6caeb375624d5acc075d
j0nathan33 <j0nathan33@live.com>
2017-03-08 15:00:49 -0500

    Fix build

--- src/main/java/com/cdancy/bitbucket/rest/domain/pullrequest/Properties.java
+++ src/main/java/com/cdancy/bitbucket/rest/domain/pullrequest/Properties.java
@@ -1,12 +1,24 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.cdancy.bitbucket.rest.domain.pullrequest;
 
-import com.cdancy.bitbucket.rest.domain.common.Error;
-import com.cdancy.bitbucket.rest.domain.common.Links;
-import com.cdancy.bitbucket.rest.utils.Utils;
 import com.google.auto.value.AutoValue;
 import org.jclouds.json.SerializedNames;
-
-import java.util.List;
 
 /**
  * Created by jdoire on 07/03/2017.
@@ -14,9 +26,10 @@ import java.util.List;
 @AutoValue
 public abstract class Properties {
     public abstract Long openTaskCount();
+
     public abstract Long resolvedTaskCount();
 
-    @SerializedNames({ "openTaskCount", "resolvedTaskCount"})
+    @SerializedNames({"openTaskCount", "resolvedTaskCount"})
     public static Properties create(long openTaskCount, long resolvedTaskCount) {
         return new AutoValue_Properties(openTaskCount, resolvedTaskCount);
     }
