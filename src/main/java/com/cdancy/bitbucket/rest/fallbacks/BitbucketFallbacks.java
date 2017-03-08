f94a1b4d93930f1d6d6508f668a3624c3b69b7cc
j0nathan33 <j0nathan33@live.com>
2017-03-08 14:49:35 -0500

    Handle exception for branchPermission

--- src/main/java/com/cdancy/bitbucket/rest/fallbacks/BitbucketFallbacks.java
+++ src/main/java/com/cdancy/bitbucket/rest/fallbacks/BitbucketFallbacks.java
@@ -23,9 +23,7 @@ import static com.google.common.base.Throwables.propagate;
 import java.util.Iterator;
 import java.util.List;
 
-import com.cdancy.bitbucket.rest.domain.branch.Branch;
-import com.cdancy.bitbucket.rest.domain.branch.BranchModel;
-import com.cdancy.bitbucket.rest.domain.branch.BranchPage;
+import com.cdancy.bitbucket.rest.domain.branch.*;
 import com.cdancy.bitbucket.rest.domain.comment.Comments;
 import com.cdancy.bitbucket.rest.domain.common.Error;
 import com.cdancy.bitbucket.rest.domain.project.Project;
@@ -83,6 +81,15 @@ public final class BitbucketFallbacks {
         public Object createOrPropagate(Throwable throwable) throws Exception {
             if (checkNotNull(throwable, "throwable") != null) {
                 return createBranchPageFromErrors(getErrors(throwable.getMessage()));
+            }
+            throw propagate(throwable);
+        }
+    }
+
+    public static final class BranchPermissionPageOnError implements Fallback<Object> {
+        public Object createOrPropagate(Throwable throwable) throws Exception {
+            if (checkNotNull(throwable, "throwable") != null) {
+                return createBranchPermissionPageFromErrors(getErrors(throwable.getMessage()));
             }
             throw propagate(throwable);
         }
@@ -215,6 +222,10 @@ public final class BitbucketFallbacks {
 
     public static BranchPage createBranchPageFromErrors(List<Error> errors) {
         return BranchPage.create(-1, -1, -1, -1, true, null, errors);
+    }
+
+    public static BranchPermissionPage createBranchPermissionPageFromErrors(List<Error> errors) {
+        return BranchPermissionPage.create(-1, -1, -1, -1, true, null, errors);
     }
 
     public static ChangePage createChangePageFromErrors(List<Error> errors) {
--- src/main/java/com/cdancy/bitbucket/rest/features/BranchApi.java
+++ src/main/java/com/cdancy/bitbucket/rest/features/BranchApi.java
@@ -120,6 +120,7 @@ public interface BranchApi {
     @Documentation({"https://developer.atlassian.com/static/rest/bitbucket-server/4.14.1/bitbucket-ref-restriction-rest.html#idm45354011023456"})
     @Consumes(MediaType.APPLICATION_JSON)
     @Path("/branch-permissions/2.0/projects/{project}/repos/{repo}/restrictions")
+    @Fallback(BitbucketFallbacks.BranchPermissionPageOnError.class)
     @GET
     BranchPermissionPage getBranchPermission(@PathParam("project") String project,
                                              @PathParam("repo") String repo,
@@ -130,6 +131,7 @@ public interface BranchApi {
     @Documentation({"https://developer.atlassian.com/static/rest/bitbucket-server/4.14.1/bitbucket-ref-restriction-rest.html#idm45354011023456"})
     @Path("/branch-permissions/2.0/projects/{project}/repos/{repo}/restrictions")
     @Produces("application/vnd.atl.bitbucket.bulk+json")
+    @Fallback(BitbucketFallbacks.FalseOnError.class)
     @POST
     boolean updateBranchPermission(@PathParam("project") String project,
                                    @PathParam("repo") String repo,
@@ -139,6 +141,7 @@ public interface BranchApi {
     @Documentation({"https://developer.atlassian.com/static/rest/bitbucket-server/4.14.1/bitbucket-ref-restriction-rest.html#idm45354011023456"})
     @Consumes(MediaType.APPLICATION_JSON)
     @Path("/branch-permissions/2.0/projects/{project}/repos/{repo}/restrictions/{id}")
+    @Fallback(BitbucketFallbacks.FalseOnError.class)
     @DELETE
     boolean deleteBranchPermission(@PathParam("project") String project,
                                    @PathParam("repo") String repo,
