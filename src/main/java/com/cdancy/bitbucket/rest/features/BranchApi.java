6477d185fbdf1724ea6577fce7f5c9e58be8a62e
j0nathan33 <j0nathan33@live.com>
2017-03-09 10:33:04 -0500

    Add mock and integration test

--- src/main/java/com/cdancy/bitbucket/rest/features/BranchApi.java
+++ src/main/java/com/cdancy/bitbucket/rest/features/BranchApi.java
@@ -135,6 +135,7 @@ public interface BranchApi {
     @Documentation({"https://developer.atlassian.com/static/rest/bitbucket-server/4.14.1/bitbucket-ref-restriction-rest.html#idm45354011023456"})
     @Path("/branch-permissions/2.0/projects/{project}/repos/{repo}/restrictions")
     @Produces("application/vnd.atl.bitbucket.bulk+json")
+    @Consumes(MediaType.APPLICATION_JSON)
     @Fallback(BitbucketFallbacks.FalseOnError.class)
     @POST
     boolean updateBranchPermission(@PathParam("project") String project,
