57675f62709d76e01767a2d37b4635b5dddb5dda
Jonathan Doire <jdoire@acquisio.com>
2017-06-06 09:35:28 -0400

    Change HardCode User to defaultUser

--- src/test/java/com/cdancy/bitbucket/rest/features/DefaultReviewersApiLiveTest.java
+++ src/test/java/com/cdancy/bitbucket/rest/features/DefaultReviewersApiLiveTest.java
@@ -60,7 +60,7 @@ public class DefaultReviewersApiLiveTest extends BaseBitbucketApiLiveTest {
         Matcher matcherSrc = Matcher.create(Matcher.MatcherId.ANY, true);
         Matcher matcherDst = Matcher.create(Matcher.MatcherId.ANY, true);
         List<User> listUser = new ArrayList<>();
-        listUser.add(User.create("test", "test@test.com", 1, "test", true, "test", "NORMAL"));
+        listUser.add(getDefaultUser());
         CreateCondition condition = CreateCondition.create(null, generatedTestContents.repository, matcherSrc,
                 matcherDst, listUser, requiredApprover);
 
