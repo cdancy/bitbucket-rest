f1927c2613c39025c1ae480bd32f85dc4e26cdfb
ChristopherDancy <christopher.dancy@pega.com>
2018-01-18 15:04:20 -0500

    Added tests surrounding use of using system properties and environment variables to configure the BitbucketClient.

--- src/main/java/com/cdancy/bitbucket/rest/BitbucketUtils.java
+++ src/main/java/com/cdancy/bitbucket/rest/BitbucketUtils.java
@@ -17,21 +17,33 @@
 
 package com.cdancy.bitbucket.rest;
 
+import static com.cdancy.bitbucket.rest.BitbucketConstants.BITBUCKET_REST_PROPERTY_ID;
+import static com.cdancy.bitbucket.rest.BitbucketConstants.BITBUCKET_REST_VARIABLE_ID;
 import static com.cdancy.bitbucket.rest.BitbucketConstants.CREDENTIALS_PROPERTIES;
 import static com.cdancy.bitbucket.rest.BitbucketConstants.DEFAULT_ENDPOINT;
 import static com.cdancy.bitbucket.rest.BitbucketConstants.ENDPOINT_PROPERTIES;
+import static com.cdancy.bitbucket.rest.BitbucketConstants.JCLOUDS_PROPERTY_ID;
+import static com.cdancy.bitbucket.rest.BitbucketConstants.JCLOUDS_VARIABLE_ID;
 import static com.cdancy.bitbucket.rest.BitbucketConstants.TOKEN_PROPERTIES;
+import com.google.common.base.Throwables;
 
 import java.util.List;
 import java.util.Map;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
+import java.lang.reflect.Field;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.Objects;
+import java.util.Properties;
 
 /**
  * Collection of static methods to be used globally.
  */
+@SuppressWarnings("PMD.TooManyStaticImports")
 public class BitbucketUtils {
 
     /**
@@ -124,7 +136,109 @@ public class BitbucketUtils {
         // 3.) If neither #1 or #2 find anything "Anonymous" access is assumed.
         return inferAuth.build();
     }
-    
+
+    /**
+     * Find jclouds overrides (e.g. Properties) first searching within System
+     * Properties and then within Environment Variables (former takes precedance).
+     * 
+     * @return Properties object with populated jclouds properties.
+     */
+    public static Properties inferOverrides() {
+        final Properties overrides = new Properties();
+
+        // 1.) Iterate over system properties looking for relevant properties.
+        final Properties systemProperties = System.getProperties();
+        final Enumeration<String> enums = (Enumeration<String>) systemProperties.propertyNames();
+        while (enums.hasMoreElements()) {
+            final String key = enums.nextElement();
+            if (key.startsWith(BITBUCKET_REST_PROPERTY_ID)) {
+                final int index = key.indexOf(JCLOUDS_PROPERTY_ID);
+                final String trimmedKey = key.substring(index, key.length());
+                overrides.put(trimmedKey, systemProperties.getProperty(key));
+            }
+        }
+
+        // 2.) Iterate over environment variables looking for relevant variables. System
+        //     Properties take precedence here so if the same property was already found
+        //     there then we don't add it or attempt to override.
+        for (final Map.Entry<String, String> entry : System.getenv().entrySet()) {
+            if (entry.getKey().startsWith(BITBUCKET_REST_VARIABLE_ID)) {
+                final int index = entry.getKey().indexOf(JCLOUDS_VARIABLE_ID);
+                final String trimmedKey = entry.getKey()
+                        .substring(index, entry.getKey().length())
+                        .toLowerCase()
+                        .replaceAll("_", ".");
+                if (!overrides.containsKey(trimmedKey)) {
+                    overrides.put(trimmedKey, entry.getValue());
+                }
+            }
+        }
+        
+        return overrides;
+    }
+
+    /**
+     * Add the passed environment variables to the currently existing env-vars.
+     * 
+     * @param addEnvVars the env-vars to add.
+     */
+    public static void addEnvironmentVariables(final Map<String, String> addEnvVars) {
+        Objects.requireNonNull(addEnvVars, "Must pass non-null Map");
+        final Map<String, String> newenv = Maps.newHashMap(System.getenv());
+        newenv.putAll(addEnvVars);
+        setEnvironmentVariables(newenv);
+    }
+
+    /**
+     * Remove the passed environment variables keys from the environment.
+     * 
+     * @param removeEnvVars the env-var keys to be removed.
+     */
+    public static void removeEnvironmentVariables(final Collection<String> removeEnvVars) {
+        Objects.requireNonNull(removeEnvVars, "Must pass non-null Collection");
+        final Map<String, String> newenv = Maps.newHashMap(System.getenv());
+        newenv.keySet().removeAll(removeEnvVars);
+        setEnvironmentVariables(newenv);
+    }
+
+    /**
+     * Re-set the environment variables with passed map.
+     * 
+     * @param newEnvVars map to reset env-vars with.
+     */
+    public static void setEnvironmentVariables(final Map<String, String> newEnvVars) {
+        Objects.requireNonNull(newEnvVars, "Must pass non-null Map");
+
+        try {
+            final Class<?> processEnvironmentClass = Class.forName("java.lang.ProcessEnvironment");
+            final Field theEnvironmentField = processEnvironmentClass.getDeclaredField("theEnvironment");
+            theEnvironmentField.setAccessible(true);
+            final Map<String, String> env = (Map<String, String>) theEnvironmentField.get(null);
+            env.putAll(newEnvVars);
+            final Field theCaseInsensitiveEnvironmentField = processEnvironmentClass.getDeclaredField("theCaseInsensitiveEnvironment");
+            theCaseInsensitiveEnvironmentField.setAccessible(true);
+            final Map<String, String> cienv = (Map<String, String>) theCaseInsensitiveEnvironmentField.get(null);
+            cienv.putAll(newEnvVars);
+        } catch (final ClassNotFoundException | IllegalAccessException | IllegalArgumentException | NoSuchFieldException | SecurityException e) {
+            final Class[] classes = Collections.class.getDeclaredClasses();
+            final Map<String, String> env = System.getenv();
+            for (final Class cl : classes) {
+                if ("java.util.Collections$UnmodifiableMap".equals(cl.getName())) {
+                    try {
+                        final Field field = cl.getDeclaredField("m");
+                        field.setAccessible(true);
+                        final Object obj = field.get(env);
+                        final Map<String, String> map = (Map<String, String>) obj;
+                        map.clear();
+                        map.putAll(newEnvVars);
+                    } catch (final NoSuchFieldException | IllegalAccessException e2) {
+                        throw Throwables.propagate(e2);
+                    }
+                }
+            }
+        }
+    }
+
     protected BitbucketUtils() {
         throw new UnsupportedOperationException("Purposefully not implemented");
     }
--- src/test/java/com/cdancy/bitbucket/rest/features/BitbucketClientLiveTest.java
+++ src/test/java/com/cdancy/bitbucket/rest/features/BitbucketClientLiveTest.java
@@ -17,6 +17,9 @@
 
 package com.cdancy.bitbucket.rest.features;
 
+import static com.cdancy.bitbucket.rest.BitbucketConstants.ENDPOINT_PROPERTIES;
+import static com.cdancy.bitbucket.rest.BitbucketConstants.CREDENTIALS_PROPERTIES;
+import static com.cdancy.bitbucket.rest.BitbucketConstants.TOKEN_PROPERTIES;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.testng.annotations.Test;
@@ -24,14 +27,26 @@ import org.testng.annotations.Test;
 import com.cdancy.bitbucket.rest.BaseBitbucketApiLiveTest;
 import com.cdancy.bitbucket.rest.BitbucketClient;
 import com.cdancy.bitbucket.rest.TestUtilities;
+import com.cdancy.bitbucket.rest.auth.AuthenticationType;
 import com.cdancy.bitbucket.rest.domain.admin.UserPage;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import javax.annotation.Nullable;
 
-@Test(groups = "live", testName = "BitbucketClientLiveTest")
+@Test(groups = "live", testName = "BitbucketClientLiveTest", singleThreaded = true)
 public class BitbucketClientLiveTest extends BaseBitbucketApiLiveTest {
+
+    private static final String DUMMY_ENDPOINT = "http://some-non-existent-host:12345";
+    private static final String SYSTEM_JCLOUDS_TIMEOUT = "bitbucket.rest.jclouds.so-timeout";
+    private static final String ENVIRONMENT_JCLOUDS_TIMEOUT = "BITBUCKET_REST_JCLOUDS_SO-TIMEOUT";
 
     @Test
     public void testCreateClient() {
-        final BitbucketClient client = new BitbucketClient(this.endpoint, this.bitbucketAuthentication);
+        final BitbucketClient client = new BitbucketClient(this.endpoint, this.bitbucketAuthentication, null);
         final UserPage userPage = client.api().adminApi().listUsers(null, 1, 1);
         assertThat(userPage).isNotNull();
         assertThat(userPage.errors()).isEmpty();
@@ -62,5 +77,221 @@ public class BitbucketClientLiveTest extends BaseBitbucketApiLiveTest {
         final UserPage userPage = client.api().adminApi().listUsers(null, 1, 1);
         assertThat(userPage).isNotNull();
         assertThat(userPage.errors()).isNotEmpty();
+    }
+
+    @Test
+    public void testCreateClientWithEndpointFromSystemProperties() {
+        clearSystemProperties();
+
+        System.setProperty(ENDPOINT_PROPERTIES.get(0), this.endpoint);
+        final BitbucketClient client = new BitbucketClient(null, this.bitbucketAuthentication, null);
+        assertThat(client.endPoint()).isEqualTo(this.endpoint);
+        final UserPage userPage = client.api().adminApi().listUsers(null, 1, 1);
+        assertThat(userPage).isNotNull();
+        assertThat(userPage.errors()).isEmpty();
+        clearSystemProperties();
+    }
+
+    @Test
+    public void testCreateClientWithWrongEndpointFromSystemProperties() {
+        clearSystemProperties();
+
+        System.setProperty(ENDPOINT_PROPERTIES.get(0), DUMMY_ENDPOINT);
+        final BitbucketClient client = new BitbucketClient(null, this.bitbucketAuthentication, null);
+        assertThat(client.endPoint()).isEqualTo(DUMMY_ENDPOINT);
+        final UserPage userPage = client.api().adminApi().listUsers(null, 1, 1);
+        assertThat(userPage).isNotNull();
+        assertThat(userPage.errors()).isNotEmpty();
+        clearSystemProperties();
+    }
+
+    @Test
+    public void testCreateClientWithAuthenticationFromSystemProperties() {
+        clearSystemProperties();
+
+        final AuthenticationType currentAuthType = this.bitbucketAuthentication.authType();
+        final String correctAuth = this.bitbucketAuthentication.authValue();
+        if (currentAuthType == AuthenticationType.Basic) {
+            System.setProperty(CREDENTIALS_PROPERTIES.get(0), correctAuth);
+        } else if (currentAuthType == AuthenticationType.Bearer) {
+            System.setProperty(TOKEN_PROPERTIES.get(0), correctAuth);
+        }
+
+        final BitbucketClient client = new BitbucketClient(this.endpoint, null, null);
+        assertThat(client.authType()).isEqualTo(currentAuthType);
+        assertThat(client.authValue()).isEqualTo(correctAuth);
+        final UserPage userPage = client.api().adminApi().listUsers(null, 1, 1);
+        assertThat(userPage).isNotNull();
+        assertThat(userPage.errors()).isEmpty();
+        clearSystemProperties();
+    }
+
+    @Test
+    public void testCreateClientWithWrongAuthenticationFromSystemProperties() {
+        clearSystemProperties();
+
+        final AuthenticationType currentAuthType = this.bitbucketAuthentication.authType();
+        final String wrongAuth = TestUtilities.randomStringLettersOnly();
+        if (currentAuthType == AuthenticationType.Basic) {
+            System.setProperty(CREDENTIALS_PROPERTIES.get(0), wrongAuth);
+        } else if (currentAuthType == AuthenticationType.Bearer) {
+            System.setProperty(TOKEN_PROPERTIES.get(0), wrongAuth);
+        }
+
+        final BitbucketClient client = new BitbucketClient(this.endpoint, null, null);
+        assertThat(client.authType()).isEqualTo(currentAuthType);
+        assertThat(client.authValue()).isEqualTo(wrongAuth);
+        final UserPage userPage = client.api().adminApi().listUsers(null, 1, 1);
+        assertThat(userPage).isNotNull();
+        assertThat(userPage.errors()).isNotEmpty();
+        clearSystemProperties();
+    }
+
+    @Test
+    public void testCreateClientWithEndpointFromEnvironmentVariables() {
+        clearEnvironmentVariables(null);
+        final Map<String, String> envVars = Maps.newHashMap();
+        envVars.put(ENDPOINT_PROPERTIES.get(2), this.endpoint);
+        TestUtilities.addEnvironmentVariables(envVars);
+
+        final BitbucketClient client = new BitbucketClient(null, this.bitbucketAuthentication, null);
+        assertThat(client.endPoint()).isEqualTo(this.endpoint);
+        final UserPage userPage = client.api().adminApi().listUsers(null, 1, 1);
+        assertThat(userPage).isNotNull();
+        assertThat(userPage.errors()).isEmpty();
+        clearEnvironmentVariables(null);
+    }
+
+    @Test
+    public void testCreateClientWithWrongEndpointFromEnvironmentVariables() {
+        clearEnvironmentVariables(null);
+        final Map<String, String> envVars = Maps.newHashMap();
+        envVars.put(ENDPOINT_PROPERTIES.get(2), DUMMY_ENDPOINT);
+        TestUtilities.addEnvironmentVariables(envVars);
+
+        final BitbucketClient client = new BitbucketClient(null, this.bitbucketAuthentication, null);
+        assertThat(client.endPoint()).isEqualTo(DUMMY_ENDPOINT);
+        final UserPage userPage = client.api().adminApi().listUsers(null, 1, 1);
+        assertThat(userPage).isNotNull();
+        assertThat(userPage.errors()).isNotEmpty();
+        clearEnvironmentVariables(null);
+    }
+
+    @Test
+    public void testCreateClientWithAuthenticationFromEnvironmentVariables() {
+        clearEnvironmentVariables(null);
+
+        final AuthenticationType currentAuthType = this.bitbucketAuthentication.authType();
+        final String correctAuth = this.bitbucketAuthentication.authValue();
+        final String correctAuthType;
+        switch (currentAuthType) {
+            case Basic:
+                correctAuthType = CREDENTIALS_PROPERTIES.get(2);
+                break;
+            case Bearer:
+                correctAuthType = TOKEN_PROPERTIES.get(2);
+                break;
+            default:
+                correctAuthType = null;
+                break;
+        }
+
+        final Map<String, String> envVars = Maps.newHashMap();
+        envVars.put(correctAuthType, correctAuth);
+        TestUtilities.addEnvironmentVariables(envVars);
+
+        final BitbucketClient client = new BitbucketClient(this.endpoint, null, null);
+        assertThat(client.authType()).isEqualTo(currentAuthType);
+        assertThat(client.authValue()).isEqualTo(correctAuth);
+        final UserPage userPage = client.api().adminApi().listUsers(null, 1, 1);
+        assertThat(userPage).isNotNull();
+        assertThat(userPage.errors()).isEmpty();
+        clearEnvironmentVariables(null);
+    }
+
+    @Test
+    public void testCreateClientWithWrongAuthenticationFromEnvironmentVariables() {
+        clearEnvironmentVariables(null);
+
+        final AuthenticationType currentAuthType = this.bitbucketAuthentication.authType();
+        final String wrongAuth = TestUtilities.randomStringLettersOnly();
+        final String correctAuthType;
+        switch (currentAuthType) {
+            case Basic:
+                correctAuthType = CREDENTIALS_PROPERTIES.get(2);
+                break;
+            case Bearer:
+                correctAuthType = TOKEN_PROPERTIES.get(2);
+                break;
+            default:
+                correctAuthType = null;
+                break;
+        }
+
+        final Map<String, String> envVars = Maps.newHashMap();
+        envVars.put(correctAuthType, wrongAuth);
+        TestUtilities.addEnvironmentVariables(envVars);
+
+        final BitbucketClient client = new BitbucketClient(this.endpoint, null, null);
+        assertThat(client.authType()).isEqualTo(currentAuthType);
+        assertThat(client.authValue()).isEqualTo(wrongAuth);
+        final UserPage userPage = client.api().adminApi().listUsers(null, 1, 1);
+        assertThat(userPage).isNotNull();
+        assertThat(userPage.errors()).isNotEmpty();
+        clearEnvironmentVariables(null);
+    }
+
+    @Test
+    public void testCreateClientWithOverridesAndFail() {
+        final Properties properties = new Properties();
+        properties.put("jclouds.so-timeout", -1);
+        final BitbucketClient client = new BitbucketClient(this.endpoint, this.bitbucketAuthentication, properties);
+        final UserPage userPage = client.api().adminApi().listUsers(null, 1, 1);
+        assertThat(userPage).isNotNull();
+        assertThat(userPage.errors()).isNotEmpty();
+        assertThat(userPage.errors().get(0).context()).contains("timeouts can't be negative");
+    }
+
+    @Test
+    public void testCreateClientWithOverridesFromSystemPropertiesAndFail() {
+        System.clearProperty(SYSTEM_JCLOUDS_TIMEOUT);
+        System.setProperty(SYSTEM_JCLOUDS_TIMEOUT, "-1");
+        final BitbucketClient client = new BitbucketClient(this.endpoint, this.bitbucketAuthentication, null);
+        final UserPage userPage = client.api().adminApi().listUsers(null, 1, 1);
+        assertThat(userPage).isNotNull();
+        assertThat(userPage.errors()).isNotEmpty();
+        assertThat(userPage.errors().get(0).context()).contains("timeouts can't be negative");
+        System.clearProperty(SYSTEM_JCLOUDS_TIMEOUT);
+    }
+
+    @Test
+    public void testCreateClientWithOverridesFromEnvironmentVariablesAndFail() {
+        final Map<String, String> envVars = Maps.newHashMap();
+        envVars.put(ENVIRONMENT_JCLOUDS_TIMEOUT, "-1");
+        TestUtilities.addEnvironmentVariables(envVars);
+
+        final BitbucketClient client = new BitbucketClient(this.endpoint, this.bitbucketAuthentication, null);
+        final UserPage userPage = client.api().adminApi().listUsers(null, 1, 1);
+        assertThat(userPage).isNotNull();
+        assertThat(userPage.errors()).isNotEmpty();
+        assertThat(userPage.errors().get(0).context()).contains("timeouts can't be negative");
+
+        clearEnvironmentVariables(envVars.keySet());
+    }
+
+    private void clearSystemProperties() {
+        System.clearProperty(ENDPOINT_PROPERTIES.get(0));
+        System.clearProperty(CREDENTIALS_PROPERTIES.get(0));
+        System.clearProperty(TOKEN_PROPERTIES.get(0));
+    }
+
+    private void clearEnvironmentVariables(@Nullable final Collection optionalKeysToClear) {
+        final List<String> envVars = Lists.newArrayList(ENDPOINT_PROPERTIES);
+        envVars.addAll(CREDENTIALS_PROPERTIES);
+        envVars.addAll(TOKEN_PROPERTIES);
+        if (optionalKeysToClear != null) {
+            envVars.addAll(optionalKeysToClear);
+        }
+        TestUtilities.removeEnvironmentVariables(envVars);
     }
 }
