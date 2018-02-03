043ef13e7e0361baaca5d52a2967d166f16eac60
dancc <Chris.Dancy@pega.com>
2018-02-02 19:10:12 -0500

    Added mock tests for TagApi.delete endpoint

--- src/test/java/com/cdancy/bitbucket/rest/features/TagApiMockTest.java
+++ src/test/java/com/cdancy/bitbucket/rest/features/TagApiMockTest.java
@@ -23,6 +23,7 @@ import com.cdancy.bitbucket.rest.BitbucketApi;
 import com.cdancy.bitbucket.rest.BitbucketApiMetadata;
 import com.cdancy.bitbucket.rest.domain.tags.Tag;
 import com.cdancy.bitbucket.rest.BaseBitbucketMockTest;
+import com.cdancy.bitbucket.rest.domain.common.RequestStatus;
 import com.cdancy.bitbucket.rest.domain.tags.TagPage;
 import com.cdancy.bitbucket.rest.options.CreateTag;
 import com.google.common.collect.ImmutableMap;
@@ -38,8 +39,10 @@ import org.testng.annotations.Test;
 public class TagApiMockTest extends BaseBitbucketMockTest {
 
     private final String getMethod = "GET";
+    private final String deleteMethod = "DELETE";
     private final String restApiPath = "/rest/api/";
     private final String reposPath = "/repos/";
+    private final String tagsPath = "/tags/";
     private final String projectsPath = "/projects/";
     private final String limitKeyword = "limit";
     private final String startKeyword = "start";
@@ -88,7 +91,7 @@ public class TagApiMockTest extends BaseBitbucketMockTest {
             final String commitHash = "8d351a10fb428c0c1239530256e21cf24f136e73";
             assertThat(commitHash.equalsIgnoreCase(tag.latestCommit())).isTrue();
             assertSent(server, getMethod, restApiPath + BitbucketApiMetadata.API_VERSION
-                    + projectsPath + projectKey + reposPath + repoKey + "/tags/" + tagName);
+                    + projectsPath + projectKey + reposPath + repoKey + tagsPath + tagName);
         } finally {
             baseApi.close();
             server.shutdown();
@@ -108,7 +111,7 @@ public class TagApiMockTest extends BaseBitbucketMockTest {
             assertThat(tag).isNotNull();
             assertThat(tag.errors()).isNotEmpty();
             assertSent(server, getMethod, restApiPath + BitbucketApiMetadata.API_VERSION
-                    + projectsPath + projectKey + reposPath + repoKey + "/tags/" + tagName);
+                    + projectsPath + projectKey + reposPath + repoKey + tagsPath + tagName);
         } finally {
             baseApi.close();
             server.shutdown();
@@ -153,6 +156,49 @@ public class TagApiMockTest extends BaseBitbucketMockTest {
             final Map<String, ?> queryParams = ImmutableMap.of(limitKeyword, 10, startKeyword, 0);
             assertSent(server, getMethod, restApiPath + BitbucketApiMetadata.API_VERSION
                     + projectsPath + projectKey + reposPath + repoKey + "/tags", queryParams);
+        } finally {
+            baseApi.close();
+            server.shutdown();
+        }
+    }
+
+    public void testDeleteTag() throws Exception {
+        final MockWebServer server = mockWebServer();
+
+        server.enqueue(new MockResponse().setResponseCode(204));
+        final BitbucketApi baseApi = api(server.getUrl("/"));
+        final TagApi api = baseApi.tagApi();
+        try {
+            final String tagName = "release-2.0.0";
+
+            final RequestStatus tag = api.delete(projectKey, repoKey, tagName);
+            assertThat(tag).isNotNull();
+            assertThat(tag.errors().isEmpty()).isTrue();
+            assertThat(tag.value()).isTrue();
+            
+            assertSent(server, deleteMethod, restApiPath + BitbucketApiMetadata.API_VERSION
+                    + projectsPath + projectKey + reposPath + repoKey + tagsPath + tagName);
+        } finally {
+            baseApi.close();
+            server.shutdown();
+        }
+    }
+
+    public void testDeleteTagNonExistent() throws Exception {
+        final MockWebServer server = mockWebServer();
+
+        server.enqueue(new MockResponse().setResponseCode(404));
+        final BitbucketApi baseApi = api(server.getUrl("/"));
+        final TagApi api = baseApi.tagApi();
+        try {
+            final String tagName = "non-existent-tag";
+
+            final RequestStatus tag = api.delete(projectKey, repoKey, tagName);
+            assertThat(tag).isNotNull();
+            assertThat(tag.errors()).isNotEmpty();
+            assertThat(tag.value()).isFalse();
+            assertSent(server, deleteMethod, restApiPath + BitbucketApiMetadata.API_VERSION
+                    + projectsPath + projectKey + reposPath + repoKey + tagsPath + tagName);
         } finally {
             baseApi.close();
             server.shutdown();
