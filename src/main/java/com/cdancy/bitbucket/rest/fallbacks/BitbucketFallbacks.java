d6fdf8e81b5a22b0fe3d685a769e75dadc3417a8
Adam Tyler <adamtyler07@gmail.com>
2019-03-15 21:28:12 -0400

    Issue-179 Implement last-modified API

--- src/main/java/com/cdancy/bitbucket/rest/fallbacks/BitbucketFallbacks.java
+++ src/main/java/com/cdancy/bitbucket/rest/fallbacks/BitbucketFallbacks.java
@@ -41,6 +41,7 @@ import com.cdancy.bitbucket.rest.domain.common.RequestStatus;
 import com.cdancy.bitbucket.rest.domain.common.Veto;
 import com.cdancy.bitbucket.rest.domain.defaultreviewers.Condition;
 import com.cdancy.bitbucket.rest.domain.file.FilesPage;
+import com.cdancy.bitbucket.rest.domain.file.LastModifiedSummary;
 import com.cdancy.bitbucket.rest.domain.file.LinePage;
 import com.cdancy.bitbucket.rest.domain.file.RawContent;
 import com.cdancy.bitbucket.rest.domain.participants.Participants;
@@ -473,6 +474,15 @@ public final class BitbucketFallbacks {
         }
     }
 
+    public static final class LastModifiedSummaryOnError implements Fallback<Object> {
+        public Object createOrPropagate(final Throwable throwable) throws Exception {
+            if (checkNotNull(throwable, "throwable") != null) {
+                return createLastModifiedSummaryFromErrors(getErrors(throwable.getMessage()));
+            }
+            throw propagate(throwable);
+        }
+    }
+
     public static final class RawContentOnError implements Fallback<Object> {
         @Override
         public Object createOrPropagate(final Throwable throwable) throws Exception {
@@ -536,6 +546,10 @@ public final class BitbucketFallbacks {
 
     public static FilesPage createFilesPageFromErrors(final List<Error> errors) {
         return FilesPage.create(-1, -1, -1, -1, true, null, errors);
+    }
+
+    public static LastModifiedSummary createLastModifiedSummaryFromErrors(final List<Error> errors) {
+        return LastModifiedSummary.create(null, null, errors);
     }
 
     public static Branch createBranchFromErrors(final List<Error> errors) {
