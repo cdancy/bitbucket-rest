eca9dcdd09f6b71b8aee8a71e466e1490ad68843
padas2 <Supreeth.Padavala@in.pega.com>
2019-06-10 13:15:00 +0530

    Fixing PMD violations

--- src/test/java/com/cdancy/bitbucket/rest/features/SupportApiLiveTest.java
+++ src/test/java/com/cdancy/bitbucket/rest/features/SupportApiLiveTest.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.cdancy.bitbucket.rest.features;
 
 import com.cdancy.bitbucket.rest.BaseBitbucketApiLiveTest;
@@ -7,25 +24,25 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 @Test(groups = "live", testName = "SupportApiLiveTest", singleThreaded = true)
-public class SupportApiLiveTest extends BaseBitbucketApiLiveTest{
+public class SupportApiLiveTest extends BaseBitbucketApiLiveTest {
     private SupportApi api() {
         return api.supportApi();
     }
 
     @Test
     public void testSupportZipCreation() {
-        SupportZipDetails details = api().createSupportZip();
+        final SupportZipDetails details = api().createSupportZip();
         assertIntegrity(details);
     }
 
     @Test
     public void testSupportZipStatus() {
-        SupportZipDetails details = api().createSupportZip();
-        SupportZipStatus supportZip = api().getSupportZipStatus(details.taskId());
+        final SupportZipDetails details = api().createSupportZip();
+        final SupportZipStatus supportZip = api().getSupportZipStatus(details.taskId());
         assertIntegrity(supportZip);
     }
 
-    private void assertIntegrity(SupportZipStatus supportZip) {
+    private void assertIntegrity(final SupportZipStatus supportZip) {
         assertTaskId(supportZip.taskId());
         assertTaskProgressMessage(supportZip.progressMessage());
         assertTaskProgressPercentage(supportZip.progressPercentage());
@@ -33,26 +50,26 @@ public class SupportApiLiveTest extends BaseBitbucketApiLiveTest{
         Assert.assertNotNull(supportZip.fileName());
     }
 
-    private void assertIntegrity(SupportZipDetails supportZipTask) {
+    private void assertIntegrity(final SupportZipDetails supportZipTask) {
         assertTaskId(supportZipTask.taskId());
         assertTaskProgressPercentage(supportZipTask.progressPercentage());
         assertTaskProgressMessage(supportZipTask.progressMessage());
         assertTaskStatus(supportZipTask.status());
     }
 
-    private void assertTaskProgressMessage(String progressMessage) {
+    private void assertTaskProgressMessage(final String progressMessage) {
         Assert.assertNotNull(progressMessage);
     }
 
-    private void assertTaskStatus(String status) {
+    private void assertTaskStatus(final String status) {
         Assert.assertTrue(!status.isEmpty());
     }
 
-    private void assertTaskId(String taskId) {
+    private void assertTaskId(final String taskId) {
         Assert.assertNotNull(taskId);
     }
 
-    private void assertTaskProgressPercentage(Integer progressPercentage) {
+    private void assertTaskProgressPercentage(final Integer progressPercentage) {
         Assert.assertNotNull(progressPercentage);
     }
 }
