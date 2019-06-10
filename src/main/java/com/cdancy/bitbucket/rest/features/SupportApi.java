e96591f576a63b207da3d2da391c7f6ada64c9d0
padas2 <Supreeth.Padavala@in.pega.com>
2019-06-10 12:38:25 +0530

    Removed all windows line endings

--- src/main/java/com/cdancy/bitbucket/rest/features/SupportApi.java
+++ src/main/java/com/cdancy/bitbucket/rest/features/SupportApi.java
@@ -5,7 +5,12 @@ import com.cdancy.bitbucket.rest.domain.support.SupportZipDetails;
 import com.cdancy.bitbucket.rest.filters.BitbucketAuthenticationFilter;
 import org.jclouds.rest.annotations.RequestFilters;
 import javax.inject.Named;
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Produces(MediaType.APPLICATION_JSON)
